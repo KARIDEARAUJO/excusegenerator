61310e97c491e107059b3c61002fba6265c8c299
karina <dearaujokarina@gmail.com>
2021-03-28 18:07:46 -0400

    generador-de-excusa

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Generador de excusa");
+  let who = ["mi padre", "mi mamá", "mi hija", "mi tio", "mi abuela"];
+  let what = ["se estrelló", "se robó", "me quitó", "se durmió", "me guardó"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando termine",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${who[randomWho]} ${what[randomWhat]} ${when[randomWhen]}`;
 };
